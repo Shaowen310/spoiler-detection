8a23e78734a6ed1e6cc8d4ce176d880925d4a50c
shaowen <s200061@e.ntu.edu.sg>
2021-04-05 14:12:44 +0000

    f1 score

--- train.py
+++ train.py
@@ -7,6 +7,8 @@ from typing import Sequence
 
 import numpy as np
 import torch
+import torch.nn.functional as F
+import sklearn.metrics as metrics
 
 from dataset import GoodreadsReviewsSpoilerDataset
 from model import SpoilerNet
@@ -28,22 +30,26 @@ with open(data_file, 'rb') as f:
 doc_label_sents = data['doc_label_sents']
 itow = data['itow']
 
+
 # %%
 # Split train, dev, test
-def train_dev_test_split(d:Sequence, train_size:float, dev_size:float):
+def train_dev_test_split(d: Sequence, train_size: float, dev_size: float):
     n_d = len(d)
     n_train = math.floor(n_d * train_size)
     n_dev = math.floor(n_d * dev_size)
     rand_idx = np.random.choice(n_d, n_d, replace=False)
     d_train = [d[idx] for idx in rand_idx[:n_train]]
-    d_dev = [d[idx] for idx in rand_idx[n_train:n_train+n_dev]]
-    d_test = [d[idx] for idx in rand_idx[n_train+n_dev:]]
+    d_dev = [d[idx] for idx in rand_idx[n_train:n_train + n_dev]]
+    d_test = [d[idx] for idx in rand_idx[n_train + n_dev:]]
     return d_train, d_dev, d_test
+
 
 d_train, d_dev, d_test = train_dev_test_split(doc_label_sents, train_portion, test_portion)
 
-ds_train = GoodreadsReviewsSpoilerDataset(doc_label_sents, itow, max_n_words, max_n_sents)
+ds_train = GoodreadsReviewsSpoilerDataset(d_train, itow, max_n_words, max_n_sents)
+ds_dev = GoodreadsReviewsSpoilerDataset(d_dev, itow, max_n_words, max_n_sents)
 dl_train = torch.utils.data.DataLoader(ds_train, batch_size=batch_size)
+dl_dev = torch.utils.data.DataLoader(ds_dev, batch_size=batch_size)
 
 
 # %% [markdown]
@@ -92,6 +98,49 @@ def train_one_epoch(epoch, model, dataloader, optimizer, criterion, device, log_
     return epoch_loss / len(dataloader)
 
 
+def f1_score(y_true, y_pred, mask, threshold=0.5):
+    y_true = y_true[np.nonzero(mask)]
+    y_pred = y_pred[np.nonzero(mask)]
+    y_pred = np.nonzero(y_pred >= threshold)
+    return metrics.f1_score(y_true, y_pred)
+
+
+def evaluate(model, dataloader, criterion, device='cpu'):
+    model.to(device)
+    criterion.to(device)
+
+    model.eval()
+
+    epoch_loss = 0
+    predss = []
+    labelss = []
+    sentmaskss = []
+    with torch.no_grad():
+        for elems, labels, sentmasks in dataloader:
+            elems = elems.to(device)
+            labels = labels.float().view(-1).to(device)
+            sentmasks = sentmasks.view(-1).to(device)
+
+            word_h0 = model.init_hidden(len(elems)).to(device)
+            sent_h0 = model.init_hidden(len(elems)).to(device)
+            preds, word_h0, sent_h0 = model(elems, word_h0, sent_h0)
+
+            loss = criterion(preds, labels)
+            loss *= sentmasks
+            loss = torch.sum(loss) / torch.count_nonzero(sentmasks)
+
+            labelss.append(labels)
+            predss.append(preds)
+            sentmaskss.append(sentmasks)
+
+    labels = torch.cat(labelss)
+    preds = torch.cat(predss)
+    sentmasks = torch.cat(sentmaskss)
+    f1 = f1_score(labels, preds, sentmasks)
+
+    return epoch_loss / len(dataloader), f1
+
+
 # %%
 cell_dim = 64
 att_dim = 32
@@ -117,5 +166,7 @@ for epoch in range(5):
     epoch_loss = train_one_epoch(epoch, model, dl_train, optimizer, criterion, device)
 
     print('| epoch {} | epoch_loss {} |'.format(epoch, epoch_loss))
+
+    evaluate(model, dl_dev, criterion)
 
 # %%
