fix df idf calculation

dataprepgr.py
@@ -11,6 +11,7 @@ import pickle
 import re
 import string
 import math
+from typing import Iterable
 
 import gdown
 from nltk.tokenize import word_tokenize
@@ -101,9 +102,12 @@ def get_label_sent_words(label_sents):
     return [(lb_s[0], get_sent_words(lb_s[1])) for lb_s in label_sents]
 
 
-def get_word_dict(wc, n_most_common):
+def get_word_dict(wc, n_most_common, freq=1):
     itow = ['<pad>', '<unk>']
-    itow.extend([word for (word, _) in wc.most_common(n_most_common)])
+    wcs = wc.most_common(n_most_common)
+    wcs = filter(lambda wc: wc[1] >= freq, wcs)
+    words = map(lambda wc: wc[0], wcs)
+    itow.extend(words)
     wtoi = {w: i for i, w in enumerate(itow)}
     logger.info("Vocabulary size = {}".format(len(wtoi)))
     return wtoi, itow
@@ -116,22 +120,22 @@ def get_doc_label_sent_encodes(label_sents, word2idx):
 
 # %%
 def word_count(records):
-    wc_review = []
+    wc_doc = []
     wc_artwork = collections.defaultdict(lambda: collections.Counter())
     wc = collections.Counter()
     for record in records:
         artwork_id = record['book_id']
-        wc_review_ = collections.Counter()
+        wc_doc_ = collections.Counter()
         for (_, words) in get_label_sent_words(record['review_sentences']):
-            wc_review_.update(words)
-        wc_review.append(wc_review_)
-        wc_artwork[artwork_id].update(wc_review_)
-        wc.update(wc_review_)
+            wc_doc_.update(words)
+        wc_doc.append(wc_doc_)
+        wc_artwork[artwork_id].update(wc_doc_)
+        wc.update(wc_doc_)
 
     logger.info('# of books: {}'.format(len(wc_artwork)))
 
     # vocab
-    return wc, wc_artwork, wc_review
+    return wc, wc_artwork, wc_doc
 
 
 def process(records, word2idx):
@@ -148,20 +152,23 @@ def prepare_invmap(doc_artwork, wc_review, wc_artwork):
     atod = collections.defaultdict(lambda: [])
     for d, a in enumerate(doc_artwork):
         atod[a].append(d)
-    wtor = collections.defaultdict(lambda: [])
+    wtod = collections.defaultdict(lambda: [])
     for r, wc in enumerate(wc_review):
         for w in wc.keys():
-            wtor[w].append(r)
+            wtod[w].append(r)
     wtoa = collections.defaultdict(lambda: [])
     for a, wc in wc_artwork.items():
         for w in wc.keys():
             wtoa[w].append(a)
-    return dict(atod), dict(wtor), dict(wtoa)
+    return dict(atod), dict(wtod), dict(wtoa)
 
 
-def df_idf(word, artwork_id, atod, wtor, wtoa):
-    d_i = len(atod[artwork_id])
-    d_wi = len(wtor[word])
+def df_idf(word, artwork_id, atod, wtod, wtoa):
+    d_i_docs = atod[artwork_id]
+    d_i = len(d_i_docs)
+    d_w_docs = wtod[word]
+    d_wi_docs = set.intersection(set(d_i_docs), set(d_w_docs))
+    d_wi = len(d_wi_docs)
     df = d_wi / d_i
     e = 1
     l_w = len(wtoa[word])
@@ -170,28 +177,32 @@ def df_idf(word, artwork_id, atod, wtor, wtoa):
     return df * iif
 
 
-def process_df_idf(doc_encode, doc_artwork, itow, atod, wtor, wtoa, log_every=1000):
+def process_df_idf(doc_encode, doc_artwork, itow, atod, wtod, wtoa, log_every=100000):
     all_df_idf = []
+    c = 0
     for i in range(len(doc_encode)):
         doc_label_sent_encodes = doc_encode[i]
         artwork_id = doc_artwork[i]
         for lb_s in doc_label_sent_encodes:
             for w in lb_s[1]:
                 word = itow[w]
-                dfidf = 0
+                dfidf = 1.
                 if word != '<unk>':
-                    dfidf = df_idf(word, artwork_id, atod, wtor, wtoa)
+                    dfidf = df_idf(word, artwork_id, atod, wtod, wtoa)
                 all_df_idf.append(dfidf)
-    all_df_idf = _stdscale.fit_transform(np.array(all_df_idf).reshape(-1,1)).ravel()
+                c += 1
+                if log_every and not c % log_every:
+                    print('Processed {} words'.format(c / log_every))
+    all_df_idf = _stdscale.fit_transform(np.array(all_df_idf).reshape(-1, 1)).ravel()
 
     doc_df_idf = []
     c = 0
     for doc_label_sent_encodes in doc_encode:
         doc = []
         for lb_s in doc_label_sent_encodes:
-            sent=[]
+            sent = []
             for w in lb_s[1]:
-                sent.append(all_df_idf[c])
+                sent.append(all_df_idf[c].item())
                 c += 1
             doc.append(sent)
         doc_df_idf.append(doc)
@@ -200,19 +211,20 @@ def process_df_idf(doc_encode, doc_artwork, itow, atod, wtor, wtoa, log_every=10
 
 
 # %%
-limit = 100000
+limit = 10000
 n_most_common = None
+freq_ge = 5
 
 logger.info('# of reviews: {}'.format(limit))
 logger.info('Getting word counts...')
-wc, wc_artwork, wc_review = word_count(generate_records(limit))
+wc, wc_artwork, wc_doc = word_count(generate_records(limit))
 logger.info('Building dictionaries...')
-wtoi, itow = get_word_dict(wc, n_most_common)
+wtoi, itow = get_word_dict(wc, n_most_common, freq_ge)
 logger.info('Encoding reviews...')
 doc_encode, doc_artwork = process(generate_records(limit), wtoi)
 logger.info('Calculating DF-IDF...')
-atod, wtor, wtoa = prepare_invmap(doc_artwork, wc_review, wc_artwork)
-doc_df_idf = process_df_idf(doc_encode, doc_artwork, itow, atod, wtor, wtoa)
+atod, wtod, wtoa = prepare_invmap(doc_artwork, wc_doc, wc_artwork)
+doc_df_idf = process_df_idf(doc_encode, doc_artwork, itow, atod, wtod, wtoa)
 logger.info('Saving...')
 obj = {
     'doc_label_sents': doc_encode,
@@ -222,8 +234,9 @@ obj = {
     'doc_artwork': doc_artwork,
     'doc_df_idf': doc_df_idf
 }
-filename = 'mappings_{}_{}'.format('all' if limit is None else str(limit),
-                                   'all' if n_most_common is None else str(n_most_common))
+filename = 'mappings_{}_{}_ge{}'.format('all' if limit is None else str(limit),
+                                        'all' if n_most_common is None else str(n_most_common),
+                                        str(freq_ge))
 with open(os.path.join(file_dir, filename + '.pkl'), 'wb') as f:
     pickle.dump(obj, f)
 

model.py
@@ -27,6 +27,7 @@ class SpoilerNet(nn.Module):
                  att_dim,
                  vocab_size,
                  emb_size,
+                 use_idf,
                  dropout_rate=0.5,
                  pretrained_emb=None):
         super().__init__()
@@ -35,9 +36,11 @@ class SpoilerNet(nn.Module):
         self.att_dim = att_dim
         self.vocab_size = vocab_size
         self.emb_size = emb_size
+        self.use_idf = use_idf
 
         self.emb_layer = nn.Embedding(vocab_size, emb_size, 0)
-        self.word_encoder = nn.GRU(emb_size + 1, cell_dim, bidirectional=True)
+        self.sentlv_word_emb_size = emb_size + 1 if use_idf else emb_size
+        self.word_encoder = nn.GRU(self.sentlv_word_emb_size, cell_dim, bidirectional=True)
         self.word_att = WordAttentionLayer(2 * cell_dim, att_dim)
         self.sent_encoder = nn.GRU(2 * cell_dim, cell_dim, bidirectional=True)
         self.out_linear = nn.Linear(2 * cell_dim, 1)
@@ -47,26 +50,29 @@ class SpoilerNet(nn.Module):
     def init_hidden(self, batch_size):
         return torch.zeros(2, batch_size, self.cell_dim)
 
-    def forward(self, x, word_h0, sent_h0, x_df_idf):
+    def forward(self, x, word_h0, sent_h0, x_df_idf=None):
         '''
         x size: (batch, sent_seq_len, word_seq_len)
         '''
         x = x.permute(1, 0, 2)
         # (sent_seq_len, batch, word_seq_len)
-        x_df_idf = x_df_idf.permute(1, 0, 2)
+        if self.use_idf:
+            x_df_idf = x_df_idf.permute(1, 0, 2)
 
         sentlv_sent_enc_list = []
         for i in range(len(x)):
             sent = x[i]
-            sent_df_idf = x_df_idf[i]
-
             sent = sent.permute(1, 0)
             # (word_seq_len, batch)
-            sent_df_idf = sent_df_idf.permute(1, 0).unsqueeze(2)
+
+            if self.use_idf:
+                sent_df_idf = x_df_idf[i]
+                sent_df_idf = sent_df_idf.permute(1, 0).unsqueeze(2)
 
             word_emb = self.emb_layer(sent)
-            word_emb_idf = torch.cat((word_emb, sent_df_idf), 2)
-            sentlv_word_encs, word_h0 = self.word_encoder(word_emb_idf, word_h0)
+            if self.use_idf:
+                word_emb = torch.cat((word_emb, sent_df_idf), 2)
+            sentlv_word_encs, word_h0 = self.word_encoder(word_emb, word_h0)
             # (word_seq_len, batch, num_directions * hidden_size)
 
             sentlv_word_encs = self.drop(sentlv_word_encs)

train.py
@@ -22,11 +22,11 @@ params = {}
 # ## Data
 # %%
 data_dir = 'data_/goodreads-reviews-spoiler'
-data_file = os.path.join(data_dir, 'mappings_100000_all.pkl')
+data_file = os.path.join(data_dir, 'mappings_10000_all_ge5.pkl')
 max_sent_len = 25
 max_doc_len = 30
 batch_size = 32
-train_portion, dev_portion = 0.5, 0.25
+train_portion, dev_portion = 0.8, 0.1
 
 params['max_sent_len'] = max_sent_len
 params['max_doc_len'] = max_doc_len
@@ -63,15 +63,17 @@ dl_dev = torch.utils.data.DataLoader(ds_dev, batch_size=batch_size)
 
 # %%
 model_name = 'spoilernet'
-cell_dim = 64
+cell_dim = 128
 att_dim = 32
 vocab_size = len(itow)
-emb_size = 50
+emb_size = 200
+use_idf = True
 
 params['cell_dim'] = cell_dim
 params['att_dim'] = att_dim
 params['vocab_size'] = vocab_size
 params['emb_size'] = emb_size
+params['use_idf'] = use_idf
 
 model_id = paramstore.add(model_name, params)
 
@@ -79,7 +81,11 @@ _logger = loggingutil.get_logger(model_id)
 
 _logger.info('Data file: {}'.format(data_file))
 
-model = SpoilerNet(cell_dim=cell_dim, att_dim=att_dim, vocab_size=vocab_size, emb_size=emb_size)
+model = SpoilerNet(cell_dim=cell_dim,
+                   att_dim=att_dim,
+                   vocab_size=vocab_size,
+                   emb_size=emb_size,
+                   use_idf=use_idf)
 criterion = torch.nn.BCEWithLogitsLoss(reduction='none')
 
 device = torch.device('cuda:0')
@@ -93,7 +99,14 @@ scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
 # %% [markdown]
 # ## Training
 # %%
-def train_one_epoch(epoch, model, dataloader, optimizer, criterion, device, log_interval=1000):
+def train_one_epoch(epoch,
+                    model,
+                    dataloader,
+                    optimizer,
+                    criterion,
+                    params,
+                    device,
+                    log_interval=1000):
     model.to(device)
     criterion.to(device)
 
@@ -107,13 +120,17 @@ def train_one_epoch(epoch, model, dataloader, optimizer, criterion, device, log_
         elems = elems.to(device)
         labels = labels.float().view(-1).to(device)
         sentmasks = sentmasks.view(-1).to(device)
-        dfidf = dfidf.to(device)
+        if params['use_idf']:
+            dfidf = dfidf.to(device)
 
         optimizer.zero_grad()
 
         word_h0 = model.init_hidden(len(elems)).to(device)
         sent_h0 = model.init_hidden(len(elems)).to(device)
-        preds, word_h0, sent_h0 = model(elems, word_h0, sent_h0, dfidf)
+        if params['use_idf']:
+            preds, word_h0, sent_h0 = model(elems, word_h0, sent_h0, dfidf)
+        else:
+            preds, word_h0, sent_h0 = model(elems, word_h0, sent_h0)
         loss = criterion(preds, labels)
         loss *= sentmasks
         loss = torch.sum(loss) / torch.count_nonzero(sentmasks)
@@ -142,7 +159,7 @@ def f1_score(y_true, y_pred, threshold=0.5):
     return metrics.f1_score(y_true, y_pred_lb)
 
 
-def evaluate(model, dataloader, criterion, device='cpu'):
+def evaluate(model, dataloader, criterion, params, device='cpu'):
     model.to(device)
     criterion.to(device)
 
@@ -157,11 +174,15 @@ def evaluate(model, dataloader, criterion, device='cpu'):
             elems = elems.to(device)
             labels = labels.float().view(-1).to(device)
             sentmasks = sentmasks.view(-1).to(device)
-            dfidf = dfidf.to(device)
+            if params['use_idf']:
+                dfidf = dfidf.to(device)
 
             word_h0 = model.init_hidden(len(elems)).to(device)
             sent_h0 = model.init_hidden(len(elems)).to(device)
-            preds, word_h0, sent_h0 = model(elems, word_h0, sent_h0, dfidf)
+            if params['use_idf']:
+                preds, word_h0, sent_h0 = model(elems, word_h0, sent_h0, dfidf)
+            else:
+                preds, word_h0, sent_h0 = model(elems, word_h0, sent_h0)
 
             loss = criterion(preds, labels)
             loss *= sentmasks
@@ -195,9 +216,9 @@ for epoch in range(n_epochs):
         break
     epoch_loss = 0
 
-    epoch_loss = train_one_epoch(epoch, model, dl_train, optimizer, criterion, device)
+    epoch_loss = train_one_epoch(epoch, model, dl_train, optimizer, criterion, params, device)
 
-    dev_loss, dev_f1, dev_roc_auc = evaluate(model, dl_dev, criterion, device)
+    dev_loss, dev_f1, dev_roc_auc = evaluate(model, dl_dev, criterion, params, device)
 
     _logger.info(
         '| epoch {} | epoch_loss {:.6f} | dev_loss {:.6f} | dev_f1 {:.3f} | dev_roc_auc {:.3f}'.